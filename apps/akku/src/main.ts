ed1d5e8a0541c9a58c83e0052f8d7607abefef56
krishnoit <krishnoit100@gmail.com>
2023-08-28 19:00:05 +0530

    new

--- apps/akku/src/main.ts
+++ apps/akku/src/main.ts
@@ -5,13 +5,23 @@
 
 import express from 'express';
 import * as path from 'path';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to akku!' });
+app.get('/api', async (req, res) => {
+  const data = await prisma.user.create({
+    data: {
+      email: 'new@gmai.com',
+      name: 'dsfds',
+    },
+  });
+  res.send({
+    data: data,
+  });
 });
 
 const port = process.env.PORT || 3333;
