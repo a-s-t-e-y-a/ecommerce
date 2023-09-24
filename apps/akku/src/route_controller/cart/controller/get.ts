6925060adcc1455cedd73f72770ef196de5dc00a
krishnoit <krishnoit100@gmail.com>
2023-09-24 20:17:59 +0530

    🐛 Bug fix cart bug fix

--- apps/akku/src/route_controller/cart/controller/get.ts
+++ apps/akku/src/route_controller/cart/controller/get.ts
@@ -10,44 +10,53 @@ const prisma = new PrismaClient();
 
 export const getAllCartItem = async (req: Authenticate, res: Response) => {
   try {
-    if(req.sessionId ==null){
-        throw new CustomError('Session is not created', 'Cart is empty ',404)
+    if (req.sessionId == null) {
+      throw new CustomError('Session is not created', 'Cart is empty ', 404);
     }
     const getSessionId = await prisma.session.findFirst({
       where: {
         userId: req.userId,
-        // session_Id:req.sessionId
+        session_Id: req.sessionId,
       },
       include: {
         cart: {
           include: {
             productId: true,
+            lId: true,
           },
         },
       },
-     
     });
-    if (!getSessionId) {
+    console.log(getSessionId);
+    if (getSessionId.cart.length == 0) {
       throw new CustomError(
-        'Session is created but cart is empty',
+        'Dont have any item in cart',
         'Cant fetch data',
         404
       );
     }
-    // if(getSessionId.cart.length ==0 ){
-    //     throw new CustomError(
-    //         'Dont have any item in cart',
-    //         'Cant fetch data',
-    //         404
-    //       );
-    // }
-    let total =0 
-    getSessionId.cart.map((info)=>{
-        total = total + info.productId.discounted_price*info.qty_frame
-    })
-    getSessionId['total_price'] = total
-    responseSuccess(res,new CustomSuccess('Data fetch successfully', getSessionId, 200))
+
+    let frame_total = 0,
+      lense_total = 0;
+    getSessionId.cart.map((info) => {
+      frame_total =
+        info.productId != null
+          ? frame_total + info.productId.discounted_price * info.qty_frame
+          : frame_total;
+      lense_total =
+        info.lId != null
+          ? lense_total + Number(info.lId.price) * info.qty_lenses
+          : lense_total;
+    });
+    getSessionId['frame_total'] = frame_total;
+    getSessionId['lenses_total'] = lense_total;
+    getSessionId['grand_total'] = frame_total + lense_total;
+    responseSuccess(
+      res,
+      new CustomSuccess('Data fetch successfully', getSessionId, 200)
+    );
   } catch (err) {
-    responseError(res,err)
+    console.log(err);
+    responseError(res, err);
   }
 };
