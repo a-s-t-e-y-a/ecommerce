c046e616208a6a2209c193ee04ad8d441bb4ae4d
krishnoit <krishnoit100@gmail.com>
2023-10-04 12:23:39 +0530

    ✨ New feature new

--- apps/akku/src/main.ts
+++ apps/akku/src/main.ts
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use('/api', mainRouter);
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
