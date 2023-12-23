4b12b4948ab66e90eb33c5395f55b2f77edaa08c
a-s-t-e-y-a <krishnoit100@gmail.com>
2023-12-23 12:28:08 +0530

    new

--- apps/akku/src/route_controller/products/controller/category/get.ts
+++ apps/akku/src/route_controller/products/controller/category/get.ts
@@ -19,25 +19,32 @@ export const categoryGetAll = async (req: Request, res: Response) => {
       responseError(res, err);
     }
   };
-  
-  export const categoryGetById = async (req: Request, res: Response) => {
-    try {
-      const categoryId = parseInt(req.params.id);
-      const category = await prisma.product_categories.findUnique({
-        where: { products_categories_id: categoryId },
-        include :{
-          products:true
-        }
-      });
-      if (!category) {
-        throw new CustomError('Category not found', 'Bad request', 404)
-      } else {
-        responseSuccess(
-          res,
-          new CustomSuccess('Fetched category successfully', category, 200)
-        );
-      }
-    } catch (err) {
-      responseError(res, err);
+
+export const categoryGetById = async (req: Request, res: Response) => {
+  try {
+    const categoryId = parseInt(req.params.id);
+    const page = req.query.page ? parseInt(req.query.page.toString()) : 1;
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize.toString()) : 10; // Adjust the default page size as needed
+
+    const category = await prisma.product_categories.findUnique({
+      where: { products_categories_id: categoryId },
+      include: {
+        products: {
+          take: pageSize,
+          skip: (page - 1) * pageSize,
+        },
+      },
+    });
+
+    if (!category) {
+      throw new CustomError('Category not found', 'Bad request', 404);
+    } else {
+      responseSuccess(
+        res,
+        new CustomSuccess('Fetched category successfully', category, 200)
+      );
     }
-  };
+  } catch (err) {
+    responseError(res, err);
+  }
+};
