593ca070df94d8f4ec33a5fe761d6427b1bb7a90
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-03-24 14:21:06 +0530

    blog

--- apps/akku/src/route_controller/blogs/controllers/post.ts
+++ apps/akku/src/route_controller/blogs/controllers/post.ts
@@ -4,21 +4,23 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const createBlog = async (req: Request, res: Response) => {
-  try {
-    const { heading, thumb, description, url, seo_title } = req.body;
-    const createdBlog = await prisma.blogs.create({
-      data: {
-        heading,
-        thumb,
-        description,
-        url,
-        seo_title,
-        created_on: new Date(), // assuming you want to set the current date/time
-      },
-    });
-    res.status(201).json(createdBlog);
-  } catch (error) {
-    console.error('Error creating blog:', error);
-    res.status(500).json({ error: 'Unable to create blog' });
-  }
+    try {
+        const { heading, thumb, description, url, seo_title } = req.body;
+        const currentDate = new Date();
+        const dateString = currentDate.toLocaleString();
+        const createdBlog = await prisma.blogs.create({
+            data: {
+                heading,
+                thumb,
+                description,
+                url,
+                seo_title,
+                created_on: dateString, // assuming you want to set the current date/time
+            },
+        });
+        res.status(201).json(createdBlog);
+    } catch (error) {
+        console.error('Error creating blog:', error);
+        res.status(500).json({ error: 'Unable to create blog' });
+    }
 };
