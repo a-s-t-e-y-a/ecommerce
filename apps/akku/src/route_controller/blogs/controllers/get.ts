e53d0ad0de9717f507e0f33c8693d5a65c74c64e
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-03-26 10:01:57 +0530

    new

--- apps/akku/src/route_controller/blogs/controllers/get.ts
+++ apps/akku/src/route_controller/blogs/controllers/get.ts
@@ -22,9 +22,11 @@ export const getAllBlogs = async (req: Request, res: Response) => {
           id: parseInt(id),
         },
       });
+
       if (!blog) {
         return res.status(404).json({ error: 'Blog not found' });
       }
+       blog['imageArray'] = `https://akkukachasma.s3.amazonaws.com/blogs/thumb/${item.thumb}`;
       res.status(200).json(blog);
     } catch (error) {
       console.error('Error fetching blog:', error);
