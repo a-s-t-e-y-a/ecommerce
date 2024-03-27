7070ece2d59c7bb4fcb60bde740af9d3df3926f5
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-03-27 14:57:01 +0530

    new

--- apps/akku/src/route_controller/products/controller/product/get.ts
+++ apps/akku/src/route_controller/products/controller/product/get.ts
@@ -7,10 +7,10 @@ import { responseError } from 'apps/akku/src/utils/responseError';
 const prisma = new PrismaClient();
 
 export async function getProductById(req: Request, res: Response) {
-  const productId = parseInt(req.params.id, 10);
+  const productId = req.params.id;
   try {
-    const product = await prisma.products.findUnique({
-      where: { p_id: productId },
+    const products = await prisma.products.findMany({
+      where: { product_url: productId },
       include:{
         shape_: true,
         product_color_:true,
@@ -24,6 +24,12 @@ export async function getProductById(req: Request, res: Response) {
     if (!product) {
       return res.status(404).json({ error: 'Product not found' });
     }
+    products.forEach((item) => {
+      const imageArray = item.product_images.split(',').map((image) => {
+        return `https://akkukachasma.s3.amazonaws.com/product_images/${image}`;
+      });
+      item['imageArray'] = imageArray;
+    });
     return res.json(product);
   } catch (error) {
     return res.status(500).json({ error: 'Error fetching product' });
